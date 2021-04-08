426bc4ea999a64aacb277b1da021ec6fab63b3d1
Vitto95 <grimaldi.vitto@gmail.com>
2021-04-08 18:28:59 +0200

    first

---
+++ js/script-mail.js
@@ -0,0 +1,11 @@
+var yourMail = "abc@asdjkas.com";
+//var yourMail = prompt("Qual è la tua mail?");
+console.log(yourMail);
+var mailList = [
+  "pincopallino@gmail.com",
+  "marcorossi01@gmail.com",
+  "andreabianchi32@gmail.com",
+  "ugodeughi@gmail.com",
+  "asdlkjsadlks@gmail.com",
+];
+console.log(mailList);

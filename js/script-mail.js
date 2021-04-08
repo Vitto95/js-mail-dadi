70e0e808f977794d864da2bf2e1d60164830c7e7
Vitto95 <grimaldi.vitto@gmail.com>
2021-04-08 18:53:35 +0200

    impostazione js mail

--- js/script-mail.js
+++ js/script-mail.js
@@ -1,4 +1,6 @@
-var yourMail = "abc@asdjkas.com";
+//dichiarazione variabili
+
+var yourMail = "pincopallino@gmail.com";
 //var yourMail = prompt("Qual è la tua mail?");
 console.log(yourMail);
 var mailList = [
@@ -7,5 +9,29 @@ var mailList = [
   "andreabianchi32@gmail.com",
   "ugodeughi@gmail.com",
   "asdlkjsadlks@gmail.com",
+  "",
 ];
+var mailFound = false;
+var msgResult = "";
+
 console.log(mailList);
+console.log(mailList.length);
+
+//ciclo per vedere se la mail è nella lista
+
+for (var i = 0; i < mailList.length; i++) {
+  if (yourMail === mailList[i]) {
+    console.log(mailList[i]);
+    mailFound = true;
+  }
+}
+
+//output esito ciclo
+
+if (mailFound) {
+  msgResult = "Email accettata. Bentornato!";
+} else {
+  msgResult = "Email non trovata :(";
+}
+
+console.log(msgResult);
